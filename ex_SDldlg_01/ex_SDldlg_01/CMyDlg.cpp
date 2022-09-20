4a60378a2233e3f0b12a2a8f96fd73f5472758f9
wuse2003 <94292942+wuse2003@users.noreply.github.com>
2022-09-20 19:01:24 +0800

    Add files via upload

---
+++ ex_SDldlg_01/ex_SDldlg_01/CMyDlg.cpp
@@ -0,0 +1,64 @@
+﻿// CMyDlg.cpp: 实现文件
+//
+
+#include "pch.h"
+#include "ex_SDldlg_01.h"
+#include "CMyDlg.h"
+#include "afxdialogex.h"
+
+
+// CMyDlg 对话框
+
+IMPLEMENT_DYNAMIC(CMyDlg, CDialogEx)
+
+CMyDlg::CMyDlg(CWnd* pParent /*=nullptr*/)
+	: CDialogEx(IDD_DIALOG1, pParent)
+{
+
+}
+
+CMyDlg::~CMyDlg()
+{
+}
+
+void CMyDlg::DoDataExchange(CDataExchange* pDX)
+{
+	CDialogEx::DoDataExchange(pDX);
+}
+
+
+BEGIN_MESSAGE_MAP(CMyDlg, CDialogEx)
+	ON_WM_LBUTTONDOWN()
+	ON_WM_RBUTTONDOWN()
+END_MESSAGE_MAP()
+
+
+// CMyDlg 消息处理程序
+
+
+void CMyDlg::OnLButtonDown(UINT nFlags, CPoint point)
+{
+	// TODO: 在此添加消息处理程序代码和/或调用默认值
+	CString str;
+
+	num++;
+	str.Format("鼠标左键按下了%d", num);
+
+	MessageBox(str, "按键次数", 1);
+
+	CDialogEx::OnLButtonDown(nFlags, point);
+}
+
+
+void CMyDlg::OnRButtonDown(UINT nFlags, CPoint point)
+{
+	// TODO: 在此添加消息处理程序代码和/或调用默认值
+
+	CString str;
+
+	num1++;
+	str.Format("鼠标右键按下了%d", num1);
+
+	MessageBox(str, "按键次数", 1);
+	CDialogEx::OnRButtonDown(nFlags, point);
+}
---
+++ ex_SDldlg_01/ex_SDldlg_01/CMyDlg.h
@@ -0,0 +1,28 @@
+﻿#pragma once
+
+
+// CMyDlg 对话框
+
+class CMyDlg : public CDialogEx
+{
+	DECLARE_DYNAMIC(CMyDlg)
+
+public:
+	CMyDlg(CWnd* pParent = nullptr);   // 标准构造函数
+	virtual ~CMyDlg();
+
+// 对话框数据
+#ifdef AFX_DESIGN_TIME
+	enum { IDD = IDD_DIALOG1 };
+#endif
+
+protected:
+	virtual void DoDataExchange(CDataExchange* pDX);    // DDX/DDV 支持
+
+	DECLARE_MESSAGE_MAP()
+public:
+	afx_msg void OnLButtonDown(UINT nFlags, CPoint point);
+	int num = 0;
+	int num1 = 0;
+	afx_msg void OnRButtonDown(UINT nFlags, CPoint point);
+};
